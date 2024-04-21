75ab315ed520429932fb48914596540c1051a20b
IVAN-GUY <kaluginivan2002@mail.ru>
2024-04-21 09:33:59 +0300

    Fix project

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -16,8 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'Default_secert_key')
 
